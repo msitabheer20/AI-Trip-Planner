b078c520f568e6906035eece308276180c13f099
msitabheer20 <msitabheerdey@gmail.com>
2025-04-14 15:42:06 +0530

    update config files

--- eslintrc.js
+++ eslintrc.js
@@ -1,16 +1,36 @@
+// .eslintrc.js
 module.exports = {
-    extends: "next/core-web-vitals",
-    rules: {
-        // Temporarily disable these rules if needed
-        "@next/next/no-img-element": "warn", // Downgrade from error to warning
-        "react/no-unescaped-entities": "warn",
-        "@typescript-eslint/no-explicit-any": "warn",
-        "@typescript-eslint/no-unused-vars": [
-            "warn",
-            {
-                argsIgnorePattern: "^_",
-                varsIgnorePattern: "^_",
-            },
-        ],
+    root: true,
+    parser: '@typescript-eslint/parser',
+    parserOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      project: ['./tsconfig.json'],
     },
-}
+    plugins: ['@typescript-eslint'],
+    extends: [
+      'eslint:recommended',
+      'plugin:@typescript-eslint/recommended',
+      'next/core-web-vitals',
+    ],
+    rules: {
+      '@next/next/no-img-element': 'warn',
+      'react/no-unescaped-entities': 'warn',
+  
+      // disable base no-unused-vars
+      'no-unused-vars': 'off',
+      // TS unused-vars as warning
+      '@typescript-eslint/no-unused-vars': [
+        'warn',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          caughtErrorsIgnorePattern: '^_',
+        },
+      ],
+  
+      // disable no-explicit-any entirely
+      '@typescript-eslint/no-explicit-any': 'off',
+    },
+  };
+  
