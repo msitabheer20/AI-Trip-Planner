429fab2ae4d54a9456b1be1f938b90939e6a864e
msitabheer20 <msitabheerdey@gmail.com>
2025-04-14 15:47:44 +0530

    change eslintrc.js file

--- eslintrc.js
+++ eslintrc.js
@@ -1,36 +1,37 @@
 // .eslintrc.js
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      project: ['./tsconfig.json'],
-    },
-    plugins: ['@typescript-eslint'],
-    extends: [
-      'eslint:recommended',
-      'plugin:@typescript-eslint/recommended',
-      'next/core-web-vitals',
+  root: true,
+  parser: '@typescript-eslint/parser',          // Use the TypeScript parser
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    project: ['./tsconfig.json'],              // Point to your tsconfig
+  },
+  plugins: ['@typescript-eslint'],              // Enable the TS plugin
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',    // Recommended TS rules
+    'next/core-web-vitals',
+  ],
+  rules: {
+    // Next.js overrides
+    '@next/next/no-img-element': 'warn',
+    'react/no-unescaped-entities': 'warn',
+
+    // Turn off the base no-unused-vars (to avoid conflict)
+    'no-unused-vars': 'off',
+
+    // TS-specific unused-vars as a warning, ignore _-prefixed names
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        caughtErrorsIgnorePattern: '^_',
+      },
     ],
-    rules: {
-      '@next/next/no-img-element': 'warn',
-      'react/no-unescaped-entities': 'warn',
-  
-      // disable base no-unused-vars
-      'no-unused-vars': 'off',
-      // TS unused-vars as warning
-      '@typescript-eslint/no-unused-vars': [
-        'warn',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          caughtErrorsIgnorePattern: '^_',
-        },
-      ],
-  
-      // disable no-explicit-any entirely
-      '@typescript-eslint/no-explicit-any': 'off',
-    },
-  };
-  
+
+    // Other TS rules
+    '@typescript-eslint/no-explicit-any': 'warn',
+  },
+};
